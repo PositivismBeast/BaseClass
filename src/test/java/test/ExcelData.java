33de6ce1a5fcb139c7527c194f33e3ed017255f0
PositivismBeast <prabhakaranm8@gmail.com>
2022-10-02 20:07:35 +0530

    new try

---
+++ src/test/java/test/ExcelData.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelData {
+	public static void main(String[] args) throws IOException {
+		File file =new File("C:\\Users\\admin\\eclipse-workspace\\test\\Excel Sheets\\ExcelPractice.xlsx");
+		FileInputStream stream= new FileInputStream(file);
+		Workbook workbook =new XSSFWorkbook(stream);
+		Sheet sheet = workbook.getSheet("Sheet1");
+//		Row row = sheet.getRow(1);		
+		for (int i = 0; i < sheet.getPhysicalNumberOfRows()  ; i++) {
+			Row row = sheet.getRow(i);
+			for (int j = 0; j <row.getPhysicalNumberOfCells(); j++) {
+				Cell cell = row.getCell(j);
+				System.out.println(cell);
+				
+			}
+		}
+		
+	}
+
+}
